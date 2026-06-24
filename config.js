73f87c3e7ace6f7a8c768f9f0910c44036658300
anatratia-beep <anatratia@gmail.com>
2026-06-24 15:45:39 +0300

    Update API keys and add JavaScript key

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// ⚠️ Remplacez ces valeurs par vos vraies clés Back4App
 const CONFIG = {
-    APP_ID: "VdI2BppWLpgguCokAMPr37d4MTC5VqwHXuaT06GI",
-    REST_API_KEY: "nhZiZZBuSvN889zjDsecIuqsjJUYvYN2IwTctQHx",
-    API_URL: "https://parseapi.back4app.com",
+    APP_ID:          "VdI2BppWLpgguCokAMPr37d4MTC5VqwHXuaT06GI",
+    REST_API_KEY:    "NUHN0aRIl6XUZB6pXVFndHIxcHG7w2S3dir3T4Qv",
+    JAVASCRIPT_KEY:  "xxvcvnyrbA49f7mZoa8J3DTvdYOhBER6bNjjhtoO",
+    API_URL:         "https://parseapi.back4app.com",
     CODE_INVITATION: "IBDAV1"
 };
