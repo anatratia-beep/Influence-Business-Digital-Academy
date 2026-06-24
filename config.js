fac21d32c2eeb37cf5c60987157abf4493c34015
anatratia-beep <anatratia@gmail.com>
2026-06-24 15:53:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     APP_ID:          "VdI2BppWLpgguCokAMPr37d4MTC5VqwHXuaT06GI",
-    REST_API_KEY:    "NUHN0aRIl6XUZB6pXVFndHIxcHG7w2S3dir3T4Qv",
+    REST_API_KEY:    "NUHN0aRIl6XUZB6pXVFndHlxcHG7w2S3dir3T4Qv",
     JAVASCRIPT_KEY:  "xxvcvnyrbA49f7mZoa8J3DTvdYOhBER6bNjjhtoO",
     API_URL:         "https://parseapi.back4app.com",
     CODE_INVITATION: "IBDAV1"
